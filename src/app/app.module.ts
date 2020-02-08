3b2d99820cd3b02137cd8058820a5ec2afe426c5
johnik666 <jan.tapajcik@yahoo.com>
2020-02-08 10:19:58 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from 'app.component';
+import { AppComponent } from './app.component';
 import { DateComponent } from './date.component';
 import { HumComponent } from './hum.component';
 import { TempComponent } from './temp.component';
